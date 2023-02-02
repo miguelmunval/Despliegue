4cec9f923621970dc7e3ad91a3316a4da154b1ec
Miguel Ángel Muñoz Valenzuela <91052453+miguelmunval@users.noreply.github.com>
2023-02-02 12:05:45 +0100

    Add files via upload

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 STACK_NAME=Miguel
+STACK_NAME=miguel
 REGION=us-east-1
 CLI_PROFILE=default
 
 EC2_INSTANCE_TYPE=t2.micro
+
+echo -e "\n=========== Desplegando  setup.yml ================="
+
+aws cloudformation deploy \
+	--region $REGION \
+	--profile $CLI_PROFILE \
+	--stack-name $STACK_NAME-setup \
+	--template-file setup.yml \
+	--no-fail-on-empty-changeset \
+	--capabilities CAPABILITY_NAMED_IAM \
 
 echo -e "\n=========== Desplegando  main.yml ================="
 
